beb41780c8ffa40247b1052cf7781f2fcaef0ea6
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-02 19:28:45 +0300

    Add == operator

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,8 +1,4 @@
-// ---------------------------------------------------------------------------------------------
-// PLEASE DO NOT MODIFY, RENAME OR REMOVE ANY OF CLASSES, METHODS AND VARIABLES BELOW.
-// YOU CAN ADD YOUR OWN METHODS AND VARIABLES TO THE EXISTING CLASSES AND USE THEM IN YOUR WORK.
-// ---------------------------------------------------------------------------------------------
-
+#include <QtGlobal>
 #include "Matrix.h"
 
 Matrix::Matrix(const std::vector<std::vector<unsigned char>> &values)
@@ -27,6 +23,9 @@ unsigned char Matrix::value(int row, int column) const
 
 unsigned char Matrix::value(const Position &position) const
 {
+    Q_ASSERT(position.row() < rowCount());
+    Q_ASSERT(position.column() < columnCount());
+
     return m_values[position.row()][position.column()];
 }
 
@@ -34,15 +33,39 @@ std::vector<std::vector<unsigned char>> Matrix::values() const
 {
     return this->m_values;
 }
+
 void Matrix::setValue(const Position &position, unsigned char value)
 {
+    Q_ASSERT(position.row() < rowCount());
+    Q_ASSERT(position.column() < columnCount());
+
     this->m_values[position.row()][position.column()] = value;
 }
 
 Sequence Matrix::sequenceByPath(const Path& path) const
 {
     std::vector<unsigned char> codes;
-    for (const Position& position: path.positions())
+    for (const Position& position: path.positions()) {
+        Q_ASSERT(position.row() < rowCount());
+        Q_ASSERT(position.column() < columnCount());
         codes.push_back(this->m_values[position.row()][position.column()]);
+    }
     return Sequence(codes, 0);
+}
+
+bool Matrix::operator==(const Matrix &other) const
+{
+    if (this->values().size() != other.values().size())
+        return false;
+
+    for (size_t row = 0; row < this->values().size(); ++row) {
+        if (this->values()[row].size() != other.values()[row].size())
+            return false;
+        for (size_t col = 0; col < this->values().size(); ++col) {
+            if (this->values()[row][col] != other.values()[row][col])
+                return false;
+        }
+    }
+
+    return true;
 }
