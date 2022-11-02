41bb8a9fa7a52370b89e3d22cfde41ba2d710db4
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-02 19:32:42 +0300

    rm Internal namespace

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,5 +1,8 @@
 #include "Utils.h"
-#include "InternalUtils.h"
+#include "MatrixUtils.h"
+#include "Search.h"
+#include "SequenceUtils.h"
+#include "PathUtils.h"
 #include <QDebug>
 
 using namespace InternalUtils;
@@ -11,7 +14,7 @@ std::vector<std::vector<Position>> Utils::adjacencyMatrix(const Matrix& matrix, 
     for (size_t seqIdx = 0; seqIdx < sequences.size(); ++seqIdx)
         for (size_t row = 0; row < matrix.rowCount(); ++row) {
             std::vector<Position> startPositionsForSequence;
-            if (isFoundInRow(matrix, Position(row, -1), sequences[seqIdx].codes()[0], startPositionsForSequence))
+            if (InternalUtils::UMatrix::isFoundInRow(matrix, Position(row, -1), sequences[seqIdx].codes()[0], startPositionsForSequence))
                 for (const Position& pos: startPositionsForSequence)
                     adjancetyMatrix[seqIdx].push_back(pos);
         }
@@ -29,7 +32,7 @@ std::vector<std::vector<Path>> Utils::findAllPurePaths(
 
     for (size_t seqIdx = 0; seqIdx < adjacencyMatrix.size(); ++seqIdx)
         for (size_t currStartPosIdx = 0; currStartPosIdx < adjacencyMatrix[seqIdx].size(); ++currStartPosIdx) {
-            std::vector<Path> purePathForCurr = DFS(matrix, sequences[seqIdx], adjacencyMatrix[seqIdx][currStartPosIdx], maxLengthPath);
+            std::vector<Path> purePathForCurr = Search::DFS(matrix, sequences[seqIdx], adjacencyMatrix[seqIdx][currStartPosIdx], maxLengthPath);
             if (!purePathForCurr.empty())
                 for (const Path& path: purePathForCurr)
                     possiblePurePaths[seqIdx].push_back(path);
@@ -53,8 +56,8 @@ std::vector<std::vector<std::pair<Path, int>>> Utils::combinePurePaths(
         std::vector<Path> possiblePathsForCurrSeq(purePaths[currSeqIdx].size());
         for (size_t pathIdx = 0; pathIdx < purePaths[currSeqIdx].size(); ++pathIdx) {
             Path currPath;
-            if (isPossibleAddWastedMovesBeforeFirstSequences(purePaths[currSeqIdx][pathIdx], matrix.columnCount(), maxLengthPath, currPath)) {
-                std::vector<std::pair<Path, int>> possiblePathsForCurrent = combinePurePath(
+            if (InternalUtils::USequence::isPossibleAddWastedMovesBeforeFirstSequences(purePaths[currSeqIdx][pathIdx], matrix.columnCount(), maxLengthPath, currPath)) {
+                std::vector<std::pair<Path, int>> possiblePathsForCurrent = InternalUtils::UPath::combinePurePath(
                             purePaths, currPath,
                             currSequence,
                             isVisitedSequence, sequences, matrix,
