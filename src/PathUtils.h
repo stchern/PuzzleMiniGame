3e6ac94ee1eec4c40f6de557c824d59b28a5e018
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-03 22:05:31 +0300

    Improved methods after testing

--- src/PathUtils.h
+++ src/PathUtils.h
@@ -14,22 +14,18 @@ namespace UPath
 std::vector<std::pair<Path, int>> combinePurePath(
         const std::vector<std::vector<Path>>& purePaths,
         const Path& currPath,
-        const Sequence& currSequence,
-        std::vector<bool>& isVisitedSequence,
         const std::vector<Sequence>& sequences,
-        const Matrix& matrix, size_t maxLengthPath, int score);
+        const Matrix& matrix, size_t maxLengthPath, int score,
+        std::vector<bool>& isVisitedSequence);
 
 Path concatenatePaths(const Path& rhsPath, const Path& lhsPath);
 bool isCreatedPathsAfterCurrent(
         const Path& currPath,
         const Path& nextPath,
-        const Sequence& currSequence,
-        const Sequence& nextSequence,
         const Matrix& matrix,
-        size_t maxLengthPath,
+        const size_t maxLengthPath,
         std::vector<Path>& outPaths);
 
-bool hasOverlapping(const Sequence& rhsPath, const Sequence& lhsPath, size_t& overlapLength);
 bool hasIntersection(const Path& rhsPath, const Path& lhsPath);
 
 
