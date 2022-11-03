e5e4c8042dbd8dd9ce609d5771bf849034c87667
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-03 22:06:45 +0300

    Added instantiation of TestUtils class

--- PuzzleRunUnitTests/main.cpp
+++ PuzzleRunUnitTests/main.cpp
@@ -1,17 +1,23 @@
 #include <QCoreApplication>
 #include "PuzzleUnitTests/testUtils.h"
+#include "PuzzleUnitTests/testPathUtils.h"
 #include "PuzzleUnitTests/testMatrixUtils.h"
+#include "PuzzleUnitTests/testSequenceUtils.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     TestUtils testUtils;
     TestMatrixUtils testMatrixUtils;
+    TestSequenceUtils testSequenceUtils;
+    TestPathUtils testPathUtils;
 
     QStringList args;
     args << "" << "-silent";
-    QTest::qExec(&testUtils, args);
-    QTest::qExec(&testMatrixUtils, args);
+//    QTest::qExec(&testUtils, args);
+//    QTest::qExec(&testMatrixUtils, args);
+//    QTest::qExec(&testSequenceUtils, args);
+    QTest::qExec(&testPathUtils, args);
 
     return 0;
 }
