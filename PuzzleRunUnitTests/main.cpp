29c37f45bee2dd9f79584d21c8e5419c3e78b03c
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-09 15:43:20 +0300

    Added PositionTest object into main:

--- PuzzleRunUnitTests/main.cpp
+++ PuzzleRunUnitTests/main.cpp
@@ -3,6 +3,7 @@
 #include "PuzzleUnitTests/testPathUtils.h"
 #include "PuzzleUnitTests/testMatrixUtils.h"
 #include "PuzzleUnitTests/testSequenceUtils.h"
+#include "PuzzleUnitTests/testPositionUtils.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,13 +12,15 @@ int main(int argc, char *argv[])
     TestMatrixUtils testMatrixUtils;
     TestSequenceUtils testSequenceUtils;
     TestPathUtils testPathUtils;
+    TestPositionUtils testPositionUtils;
 
     QStringList args;
     args << "" << "-silent";
 //    QTest::qExec(&testUtils, args);
 //    QTest::qExec(&testMatrixUtils, args);
 //    QTest::qExec(&testSequenceUtils, args);
-    QTest::qExec(&testPathUtils, args);
+//    QTest::qExec(&testPathUtils, args);
+    QTest::qExec(&testPositionUtils, args);
 
     return 0;
 }
